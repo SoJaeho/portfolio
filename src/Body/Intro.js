35aa06f57dd93a6334ce7d8efd250ada31bf97b4
SoJaeho <sojh615@naver.com>
2024-05-06 02:00:16 +0900

    [Intro] 윈도우XP 꾸미기

--- src/Body/Intro.js
+++ src/Body/Intro.js
@@ -1,17 +1,25 @@
+import styled from "styled-components";
 import Memo from "../components/memo/memo";
-
-
+import IntroFooter from "../components/footer/IntroFooter";
 
 function Intro(){
     
     return(
         <>
-            <div>
+            <Div>
                 <Memo/>
-
-            </div>
+                <IntroFooter/>
+            </Div>
         </>
     );
 }
 
 export default Intro;
+
+const Div = styled.div`
+   
+    position:relative;
+    background: url(https://i.imgur.com/Zk6TR5k.jpg) center center;
+    ;
+    
+`;
